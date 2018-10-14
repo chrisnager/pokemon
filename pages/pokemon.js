16f3eda4f47db1bd11e6e6bd1c08607a445ea82a
Chris Nager <cnager@gmail.com>
2018-10-14 00:54:28 -0400

    Begin adding evolution

--- pages/pokemon.js
+++ pages/pokemon.js
@@ -4,25 +4,35 @@ import { withRouter } from 'next/router'
 import { get } from '../services/api'
 import Ops from '../config/ops'
 
-const Pokemon = ({ router, data }) => (
+const Pokemon = ({ router, data, evolution }) => (
   <div>
     <nav>
       <Link href="/">
         <a>Home</a>
       </Link>
     </nav>
-    <h1>{router.query.id}</h1>
+    <h1>{data.name}</h1>
     {Object.values(data.sprites)
       .filter(i => i)
       .map((src, index) => (
         <img key={src} alt={`${router.query.id} stance ${index + 1}`} {...{ src }} />
       ))}
+
+    {evolution && (
+      <div>
+        Evolves to{' '}
+        <Link as={`/p/${evolution}`} href={`/pokemon?id=${evolution}`}>
+          <a>{evolution}</a>
+        </Link>
+      </div>
+    )}
   </div>
 )
 
 export default compose(
   withRouter,
   withState('data', 'updateData', { sprites: {} }),
+  withState('evolution', 'updateEvolution', ''),
   withHandlers({
     getPokemon: ({ updateData }) => async id => {
       try {
@@ -31,11 +41,20 @@ export default compose(
       } catch (error) {
         console.log(error)
       }
+    },
+    getEvolution: ({ updateEvolution }) => async id => {
+      try {
+        const { data } = await get(Ops.Evolution.getEvolutionById(id))
+        updateEvolution(data.chain.evolves_to[0].species.name)
+      } catch (error) {
+        console.log(error)
+      }
     }
   }),
   lifecycle({
     componentDidMount() {
       this.props.getPokemon(this.props.router.query.id)
+      this.props.getEvolution(this.props.router.query.id)
     }
   })
 )(Pokemon)
